d140921fb7d6642b7e14e7e646c800565714fda9
Master Joy <386442876@qq.com>
2023-05-11 11:02:10 +0800

    添加:几个常用的组件

---
+++ src/components/common/table-template.vue
@@ -0,0 +1,18 @@
+<template>
+    <el-table>
+        <el-table-column align="center"
+                         label="状态">
+            <template slot-scope="{row}">
+                <el-tag v-if="row.status == 1"
+                        type="success">正常</el-tag>
+                <el-tag v-else
+                        type="danger">停封</el-tag>
+            </template>
+        </el-table-column>
+    </el-table>
+</template>
+<script>
+export default {
+
+}
+</script>
