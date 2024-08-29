6ac94e050a94e13ed70aecc215819e9d8a5c6e20
Master Joy <386442876@qq.com>
2024-08-29 09:22:00 +0800

    优化：图片上传

--- src/components/SingleImgUpload/index.vue
+++ src/components/SingleImgUpload/index.vue
@@ -1,6 +1,8 @@
 <template>
-    <el-upload :action="uploadUrl"
-               :headers="{'Authorization': token}"
+    <el-upload :action="action"
+               :headers="localHeader"
+               :data="data"
+               :accept="accept"
                list-type="picture-card"
                :show-file-list="false"
                :on-success="uploadSuccess">
@@ -25,23 +27,46 @@
 </template>
 
 <script setup>
+import { computed } from 'vue'
 import useUserStore from '@/store/user'
 import { Plus } from '@element-plus/icons-vue'
 
 const uStore = useUserStore()
 const { token } = uStore
-const uploadUrl = import.meta.env.VITE_APP_BASE_API + '/upload/image'
 
 const modelValue = defineModel({ type: String })
 
+const localHeader = computed(() => {
+    if (Object.keys(props.headers).length == 0) {
+        return { 'Authorization': token }
+    }
+    return props.headers
+})
+
+const props = defineProps({
+    action: {
+        type: String,
+        default: import.meta.env.VITE_APP_BASE_API + '/upload/image'
+    },
+    headers: Object,
+    data: Object,
+    accept: {
+        type: String,
+        default: 'image/png, image/jpeg'
+    }
+})
+
+const emit = defineEmits(['uploadSuccess', 'uploadRemove'])
 // 上传
 const uploadSuccess = ({ data }) => {
     modelValue.value = data
+    emit('uploadSuccess', data)
 }
 
 // 删除
 const uploadRemove = () => {
     modelValue.value = ''
+    emit('uploadRemove')
 }
 </script>
 
