08b0a9d2dc8fe4692854c121c3d7ebfd9847cb34
Master Joy <386442876@qq.com>
2025-11-28 16:02:44 +0800

    优化：使用新验证码逻辑

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -32,22 +32,14 @@
                     </template>
                 </el-input>
             </el-form-item>
-            <el-form-item prop="code"
-                          :rules="{ required: true, trigger: 'blur', len: 4, message: '请输入正确的验证码长度' }">
-                <el-input v-model="loginForm.code"
-                          placeholder="验证码"
-                          @keyup.enter="handleLogin()">
-                    <template #prepend>
-                        <svg-icon icon-class="verify" />
-                    </template>
-                </el-input>
-                <span class="show-virify"
-                      @click="getCode()">
-                    <img :src="codeImage">
-                </span>
-            </el-form-item>
+            <Slide class="captcha-container"
+                   :data="captchaData"
+                   v-show="captchaData.show"
+                   ref="captchaRef"
+                   :events="captchaEvent" />
             <el-button :loading="loading"
                        type="primary"
+                       size="large"
                        style="width:100%;margin-bottom:30px;"
                        @click.prevent="handleLogin()">登录</el-button>
         </el-form>
@@ -56,9 +48,11 @@
 
 <script setup>
 import { getCodeApi } from './api'
-import { ref, watch, onMounted } from 'vue'
+import { ref, watch } from 'vue'
 import { useRouter, useRoute } from 'vue-router'
 import useUserStore from '@/store/user'
+import 'go-captcha-vue/dist/style.css'
+import { Slide } from 'go-captcha-vue'
 
 const route = useRoute()
 const router = useRouter()
@@ -71,31 +65,52 @@ const loginForm = ref({
     codeId: '',
     code: ''
 })
-const codeImage = ref()
+
 const loading = ref(false)
 const redirect = ref()
-
-onMounted(() => getCode())
 
 // 登录操作
 const handleLogin = async () => {
     await loginFormRef.value.validate()
+    getCode()
+    captchaData.value.show = true
+    loading.value = true
+}
+
+// 验证码
+const captchaData = ref({ show: false })
+const captchaRef = ref(null)
+const getCode = async () => {
+    const { data } = await getCodeApi()
+    loginForm.value.codeId = data.captchaKey
+    captchaData.value.thumbX = data.tileX
+    captchaData.value.thumbY = data.tileY
+    captchaData.value.thumbWidth = data.tileWidth
+    captchaData.value.thumbHeight = data.tileHeight
+    captchaData.value.image = data.imageBase64
+    captchaData.value.thumb = data.tileBase64
+    captchaRef.value.reset()
+}
+const captchaConfirm = async (point, reset) => {
+    await loginFormRef.value.validate()
     loading.value = true
     try {
+        loginForm.value.code = point.x + ',' + point.y
         await userStore.login(loginForm.value)
         router.push({ path: redirect.value || '/' })
     } finally {
         getCode()
-        loading.value = false
     }
 }
 
-// 获取验证码
-const getCode = async () => {
-    const { data } = await getCodeApi()
-    loginForm.value.codeId = data.id
-    codeImage.value = data.image
-}
+const captchaEvent = ref({
+    confirm: captchaConfirm,
+    refresh: getCode,
+    close: () => {
+        captchaData.value.show = false
+        loading.value = false
+    }
+})
 
 // 监听路由变化
 watch(
@@ -149,21 +164,12 @@ watch(
         }
     }
 
-    .show-virify {
-        position: absolute;
-        right: 2px;
-        top: 2px;
-        bottom: 2px;
-        font-size: 16px;
-        color: #889aa4;
-        cursor: pointer;
-        user-select: none;
-
-        img {
-            border-radius: 2px;
-            width: 100%;
-            height: 100%;
-        }
+    .captcha-container {
+        position: fixed;
+        left: 50%;
+        top: 50%;
+        transform: translate(-50%, -50%);
+        z-index: 1;
     }
 }
 </style>
